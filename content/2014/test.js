08db73f7d24398a3e7b75e1a065cb9b52dc584d7
jinjianhua <805001458@qq.com>
2016-12-04 11:34:27 +0800

    增加了一些笔记

---
+++ content/2014/test.js
@@ -0,0 +1,8 @@
+function foo(name,height) {
+	console.log(name + height);
+	console.log(hehe);
+	
+}
+var a = 123;
+var b= "123";
+
---
+++ content/2015/2015.js
@@ -0,0 +1,9 @@
+var content = {
+	"name":"全屏滚动",
+	"id" : "2015",
+	"type":"jquery",
+	"origin" :"项目需求",
+	"description":"应用jquery实现一个可以设置水平垂直滚动的插件",
+	"time":"2016-11-31" ,
+	"tags":"jquery 全屏滚动 css3"
+}
