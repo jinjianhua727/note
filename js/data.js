5d17503020375d171ad20134dccec776536ea5c2
jinjianhua <805001458@qq.com>
2016-12-16 18:35:20 +0800

    禁止滚动条滚动和解除滚动的笔记

--- js/data.js
+++ js/data.js
@@ -61,5 +61,14 @@ var notes = [
 		"description":"应用jquery实现一个可以设置水平垂直滚动的插件",
 		"time":"2016-11-31" ,
 		"tags":"jquery 全屏滚动 css3"
+	},
+	{
+		"name":"禁止和启动滚动条滚动",
+		"id" : "2016",
+		"type":"js",
+		"origin" :"项目需求",
+		"description":"解决弹窗遮罩层下面还能滚动，以及其他和滚动条相关的功能",
+		"time":"2016-12-14" ,
+		"tags":"js 滚动条"
 	}
 ]
