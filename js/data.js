7c34cc70c5416ef709a0df998c3e20610cba6a8c
jinjianhua <805001458@qq.com>
2017-01-03 18:35:05 +0800

    添加笔记

--- js/data.js
+++ js/data.js
@@ -70,5 +70,14 @@ var notes = [
 		"description":"解决弹窗遮罩层下面还能滚动，以及其他和滚动条相关的功能",
 		"time":"2016-12-14" ,
 		"tags":"js 滚动条"
+	},
+	{
+		"name":"关于Reset.css和Normalize.css",
+		"id" : "2018",
+		"type":"css",
+		"origin" :"工作需求",
+		"description":"Reset.css关注跨浏览器一致性，Normalize.css更倾向于覆盖大部分浏览器异同的默认样式。",
+		"time":"2017-01-03" ,
+		"tags":"css"
 	}
 ]
