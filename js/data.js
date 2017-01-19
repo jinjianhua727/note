28e95961fc52a6de70b1beb83395ef3dbec44155
jinjianhua727 <805001458@qq.com>
2017-01-19 18:03:11 +0800

    添加笔记

--- js/data.js
+++ js/data.js
@@ -79,5 +79,41 @@ var notes = [
 		"description":"Reset.css关注跨浏览器一致性，Normalize.css更倾向于覆盖大部分浏览器异同的默认样式。",
 		"time":"2017-01-03" ,
 		"tags":"css"
+	},
+	{
+		"name":"sass与compass快速入门",
+		"id" : "2019",
+		"type":"css",
+		"origin" :"项目需求",
+		"description":"sass与compass安装、使用、特性、编译配置",
+		"time":"2016-9-27" ,
+		"tags":"sass compass"
+	},
+	{
+		"name":"快速理解apply与call",
+		"id" : "2020",
+		"type":"js",
+		"origin" :"工作需求",
+		"description":"一看就忘的apply与call",
+		"time":"2016-1-16" ,
+		"tags":"js"
+	},
+	{
+		"name":"利用http-server创建零配置简单的HTTP服务器",
+		"id" : "2021",
+		"type":"nodejs",
+		"origin" :"项目需求",
+		"description":"零配置、简单、命令行http服务器",
+		"time":"2016-1-16" ,
+		"tags":"nodejs"
+	},
+	{
+		"name":"星级评分效果",
+		"id" : "2022",
+		"type":"jquery",
+		"origin" :"项目需求",
+		"description":"点击为确认评分，鼠标hover和click有样式变化",
+		"time":"2016-1-17" ,
+		"tags":"jquery"
 	}
 ]
