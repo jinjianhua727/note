ae9bafec408107f0afb646860d89e5c34346554d
jinjianhua727 <805001458@qq.com>
2017-03-10 10:50:54 +0800

    webpack2（二）entry与output

---
+++ src/content/2044/2044.js
@@ -0,0 +1,9 @@
+var content = {
+	"name":"webpack2（一）entry与output",
+	"id" : "2044",
+	"type":"react",
+	"origin" :"工作需求",
+	"description":"打包输出单个文件，打包输出多个文件，通过package.json配置webpack命令",
+	"time":"2017-3-10" ,
+	"tags":"react webpack"
+}
