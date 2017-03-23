5075b426223e9d081a9a7c39438d07150991aedd
jinjianhua727 <805001458@qq.com>
2017-03-23 14:03:13 +0800

    mark react component lifeCycle

---
+++ static/content/2048/2048.js
@@ -0,0 +1,9 @@
+var content = {
+	"name":"react 组件生命周期（附演示demo）",
+	"id" : "2048",
+	"type":"react",
+	"origin" :"工作需求",
+	"description":"组件从创建到消失的过程，可以分为三个节点：Mounted、Update、Unmount...",
+	"time":"2017-3-23" ,
+	"tags":"react"
+}
