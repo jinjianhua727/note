48c4ae56597444587c9628688b239434c357ab52
jinjianhua727 <805001458@qq.com>
2017-04-01 07:56:04 +0800

    mark js object copy

---
+++ static/content/2050/2050.js
@@ -0,0 +1,9 @@
+var content = {
+	"name":"浅拷贝与深拷贝",
+	"id" : "2050",
+	"type":"js",
+	"origin" :"工作需求",
+	"description":"Object.assign与for in实现浅拷贝，for in实现深拷贝...",
+	"time":"2017-4-1" ,
+	"tags":"js es6"
+}
